6c4d2317e4ed05a0584ea648e666bb046a477d19
jacki <2788307@clases.edu.sv>
2024-07-30 22:10:58 -0600

    Agregar archivos de proyecto.

---
+++ LaBrochaGorda/ViewsModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaBrochaGorda.ViewsModels
+{
+    public partial class MainViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private double _Alto;
+      
+        [ObservableProperty]
+        private double _Largo;
+        [ObservableProperty]
+        private string? _R1;
+        [ObservableProperty]
+        private string? _R2;
+       
+        [ObservableProperty]
+        private double _costototal;
+        [ObservableProperty]
+        private double _PrecioM2 = 2;
+
+        [RelayCommand]
+
+        public void Calcular()
+        {
+
+            double Medir = Largo * Alto;
+            double costototal = Medir * PrecioM2;
+            R1 = Medir.ToString("F2");
+            R2 = costototal.ToString("F3");
+           
+        }
+    }
+}
